b9627b62232e17e2ad7f043aba7d595f5ff1afc6
rootoftheproblem <rootoftheproblem@gmail.com>
2026-04-08 07:59:02 -0400

    Update consts.ts
    title and description changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "No! Don't Do It!";
+export const SITE_DESCRIPTION = "hiding over here I see.";
